baf2c1614f3b1bd9bc1f8aa5bc5e025b4b81b90f
Nicolas MF <nikolamf@hotmail.com>
2021-04-06 16:44:57 +0200

    Fix create.bot

--- easy-git-project.plugin.zsh
+++ easy-git-project.plugin.zsh
@@ -115,47 +115,47 @@ create.bot() {
 }
 EOF
     cat >>  index.js << EOF 
-    const Discord = require('discord.js');
-    const fs = require('fs');
+const Discord = require('discord.js');
+const fs = require('fs');
 
-    require('dotenv').config();
-    const prefix = process.env.PREFIX;
-    const owner = process.env.OWNER;
-    const token = process.env.TOKEN;
+require('dotenv').config();
+const prefix = process.env.PREFIX;
+const owner = process.env.OWNER;
+const token = process.env.TOKEN;
 
-    const client = new Discord.Client();
-    client.commands = new Discord.Collection();
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
-    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-    for (const file of commandFiles) {
-        const command = require(`./commands/${file}`);
-        client.commands.set(command.name, command);
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+client.on('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName)) return;
+
+    const command = client.commands.get(commandName);
+
+    try {
+        command.execute(client, message, args);
     }
+    catch (error) {
+        message.reply('Error: ' + error);
+    }
+});
 
-    client.on('ready', () => {
-        console.log('Ready!');
-    });
-
-    client.on('message', message => {
-        if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-        const args = message.content.slice(prefix.length).trim().split(/ +/);
-        const commandName = args.shift().toLowerCase();
-
-        if (!client.commands.has(commandName)) return;
-
-        const command = client.commands.get(commandName);
-
-        try {
-            command.execute(client, message, args);
-        }
-        catch (error) {
-            message.reply('Error: ' + error);
-        }
-    })
-
-    client.login(token);
+client.login(token);
 EOF
 
     cat >> .env << EOF
